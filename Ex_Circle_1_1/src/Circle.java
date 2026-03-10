1baf196e48d4a98d0cb8c55bf9b7d11d92374bd3
duyp66cntt-collab <duy.p.66cntt@ntu.edu.vn>
2026-03-10 21:24:04 +0700

    Ex_Circel_1_2

--- Ex_Circle_1_1/src/Circle.java
+++ Ex_Circle_1_1/src/Circle.java
@@ -11,11 +11,32 @@ public class Circle {
 		color="red";
 	
 	}
-	public double getRadius() {
+	public double getradius() {
 		return radius;
 	}
+	public Circle(double radius, String color) {
+	      this.radius = radius;
+	      this.color = color;
+	   }
+	public double getRadius() {
+	     return radius; 
+	   }
+	public void setRadius(double radius) {
+	      this.radius = radius;
+	   }
+
+	   public String getColor() {
+	      return color;
+	   }
+
+	   public void setColor(String color) {
+	      this.color = color;
+	   }
 	public double getArea() {
 		return radius*radius*Math.PI;
 	}
+	public double getCircumference() {
+		   return 2 * Math.PI * radius; 
+		}
 
 }
