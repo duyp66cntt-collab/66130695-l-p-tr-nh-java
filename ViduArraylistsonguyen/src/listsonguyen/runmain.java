49f2e97924dd0c92f6a176423fe51e532af7d54c
duyp66cntt-collab <duy.p.66cntt@ntu.edu.vn>
2026-03-14 01:54:09 +0700

    Được chức năng 1.Nhập danh sách

--- ViduArraylistsonguyen/src/listsonguyen/runmain.java
+++ ViduArraylistsonguyen/src/listsonguyen/runmain.java
@@ -1,9 +1,21 @@
 package listsonguyen;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class runmain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<Integer> dsSoNguyen = new ArrayList<Integer>();
+		Scanner banPhim = new Scanner(System.in);
+		for(int i=0;i<10;i++) {
+		System.out.print("Moi nhap phan tu thu : " + i + ":");
+		Integer tam = banPhim.nextInt();
+		dsSoNguyen.add(tam);
+		}
+		
+		
+		
 
 	}
 
